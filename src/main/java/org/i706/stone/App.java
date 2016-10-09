c0096257f51136af37ff0231890af5ad9949805f
Stone He <stone.he@ericsson.com>
2016-10-10 00:38:29 +0800

    1010101001

--- src/main/java/org/i706/stone/App.java
+++ src/main/java/org/i706/stone/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello 10101010!" );
+        System.out.println( "Hello 1010101001!" );
     }
 }
